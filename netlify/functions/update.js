73e9bd8d43914a283ce77d31f3411d254abf1c49
marten-mertan <mertangp@bk.ru>
2023-09-17 01:44:44 +0300

    add log

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,7 +1,10 @@
 const sendMessage = require("../../src/functions/sendMessage");
 
 exports.handler = async (event) => {
+  console.log('Chat object: ', JSON.parse(event.body));
+
   const { message } = JSON.parse(event.body);
   await sendMessage(message.chat.id, "Сообщение принято!");
+  
   return { statusCode: 200 };
 };
